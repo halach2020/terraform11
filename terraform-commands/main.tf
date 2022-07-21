5142e926be929cb2d0c4a2a5dd2a954261795f7f
halach2020 <azatpenji@gmail.com>
2022-07-21 06:42:27 +0000

    console

--- terraform-commands/main.tf
+++ terraform-commands/main.tf
@@ -22,6 +22,15 @@ resource "aws_instance" "tf-ec2" {
   }
 }
 
+resource "aws_instance" "tf-ec2-2" {
+  ami           = aws_instance.tf-ec2.ami # value comes from terraform console
+  instance_type = "t2.micro"
+  key_name      = "demo"    # write your pem file without .pem extension>
+  tags = {
+    "Name" = "tf-ec2"
+  }
+}
+
 resource "aws_s3_bucket" "tf-s3" {
   bucket = "azat-tf-test-bucket-addwhateveryouwant"
 }
