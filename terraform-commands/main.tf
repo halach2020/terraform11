3988c11257c8827a374984215be52c6b3693a176
halach2020 <azatpenji@gmail.com>
2022-07-21 07:34:12 +0000

    added output

--- terraform-commands/main.tf
+++ terraform-commands/main.tf
@@ -43,3 +43,11 @@ resource "aws_s3_object" "bootstrap" {
   source = "cloud"
   etag = filemd5("cloud")  
 }
+
+output "tf_example_public_ip" {
+  value = aws_instance.tf-ec2.public_ip
+}
+
+output "tf_example_s3_meta" {
+  value = aws_s3_bucket.tf-s3.region
+}
